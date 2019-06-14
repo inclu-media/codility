d9dc6aa3f602706e4be6b28688b93ed370b8dab1
Martin Melcher <martin@inclumedia.at>
2019-06-14 23:51:12 +0200

    CountDiv

--- 5_PrefixSums/CountDiv.js
+++ 5_PrefixSums/CountDiv.js
@@ -1,5 +1,17 @@
+/**
+ * A=a*K, B=b*K
+ * a'=floor(A/K), b'=floor(B/K)
+ * result=b'-a'
+ * O(1)
+ * 
+ * Ex1: A=10, B=30, K=10 => a'=1, b'=3, result=2 (wrong -> +1)
+ * Ex2: A=17, B=25, K=5 => a'=3, b'=5, result=2 (20, 25)
+ * Ex3: A=17, B=22, K=5 => a'=3, b'=4, result=1 (20)
+ */
 function solution(A, B ,K) {
-    return 3
+    let res = Math.floor(B/K) - Math.floor(A/K)
+    if (A % K == 0) res++
+    return res
 }
 
 module.exports  = solution
--- test.js
+++ test.js
@@ -7,5 +7,11 @@ describe('Lesson 5 - Prefix Sum', () => {
         it('demo data', () => {
             assert.equal(countDiv(6, 11, 2), 3)
         })
+        it('A == B', () => {
+            assert.equal(countDiv(7, 7, 1), 1)
+        })
+        it('K is prime (7919)', () => {
+            assert.equal(countDiv(0, 10000, 7919), 2)
+        })
     })
 })
